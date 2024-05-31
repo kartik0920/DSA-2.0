f2ca1e4f8b91ed9d0982b6aad093bed2c4594690
kartik0920 <kartik0920@gmail.com>
2024-05-31 20:13:14 +0530

    Empty-Commit

---
+++ Lec 60/circularq.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+class MyQueue
+{
+private:
+    int *arr;
+    int size, front, rear;
+
+public:
+    MyQueue(int n)
+    {
+        this->size = n;
+        arr = new int[size];
+        front = 0;
+        rear = 0;
+    }
+    void push(int x)
+    {
+    }
+    void pop()
+    {
+    }
+    bool IsEmpty()
+    {
+    }
+    int queuesize()
+    {
+    }
+};
+int main()
+{
+
+    return 0;
+}
